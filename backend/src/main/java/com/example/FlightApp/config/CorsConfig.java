4b4eef24c06d8a9514a47d379b7363345efa6108
Meeli <meeli.sirvi@tptlive.ee>
2025-06-24 18:10:28 +0300

    Cors for frontend

--- backend/src/main/java/com/example/FlightApp/config/CorsConfig.java
+++ backend/src/main/java/com/example/FlightApp/config/CorsConfig.java
@@ -10,8 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:4200")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:4200", "https://flightapp-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
